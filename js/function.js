914b77806d0c0cfc6f559da853e6f838b47b5b9d
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-08-15 17:09:49 -0300

    Video

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
 function hfindex(){
     $("#header").load("/ExpoferiaOnline/HeaderFooter/header.html"); 
-    $("#footer").load("/ExpoferiaOnline/HeaderFooter/footer.html"); 
+    $("#footer").load("/ExpoferiaOnline/HeaderFooter/footer.html");
 };
