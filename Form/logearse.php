ab7bf87beef4d7f6410f957eb4e2795286d669a7
ElTomy <tomassosa1303@gmail.com>
2020-08-18 22:06:56 -0300

    .

---
+++ Form/logearse.php
@@ -0,0 +1,19 @@
+<?php
+include '../conexion.php';
+if(isset($_POST['user']) && $_POST['contra']){
+        $sql = "SELECT * FROM usuario WHERE Usuario = '".$_POST['user']."'";
+        $result = $mysqli -> query($sql);
+        $ss = mysqli_fetch_array($result,MYSQLI_ASSOC);
+        if ($result) {
+                if($ss["contraseña"]==$_POST['contra']){
+                        session_start();
+                        $_SESSION['usuario']=$_POST['user'];
+                        header('Location: ../index.html');
+               
+        }else{ 
+                header("Location: ../Form/login.html");            
+                echo '<script language="javascript">alert("El usuario o la contra esta mal!");</script>';
+        }
+}
+}
+?>
