3cafb2348ba4a71ca86f7784c68ba9a3b68cd4da
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-08-19 16:14:18 -0300

    Menu

---
+++ js/menu.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+    const burger = document.querySelector('.MobileBars');
+    const nav = document.querySelector('.nav-mobile');
+    const navLinks = document.querySelectorAll('.nav-mobile li')
+
+    burger.addEventListener('click', () => {
+
+        nav.classList.toggle('nav-active');
+    });
+}
+
+navSlide();
