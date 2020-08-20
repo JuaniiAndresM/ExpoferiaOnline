00cd8b8b7249ed9b0b150804370fad66d7f7cd93
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-08-19 22:08:14 -0300

    Cambios

--- js/function.js
+++ js/function.js
@@ -1,4 +1,4 @@
-function hfindex(){
-    $("#header").load("/ExpoferiaOnline/HeaderFooter/header.html"); 
-    $("#footer").load("/ExpoferiaOnline/HeaderFooter/footer.html");
-};
+function hfindex() {
+  $("#header").load("/ExpoferiaOnline/HeaderFooter/header.html");
+  $("#footer").load("/ExpoferiaOnline/HeaderFooter/footer.html");
+}
--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,11 @@
 const navSlide = () => {
-    const burger = document.querySelector('.MobileBars');
-    const nav = document.querySelector('.nav-mobile');
-    const navLinks = document.querySelectorAll('.nav-mobile li')
+  const burger = document.querySelector(".MobileBars");
+  const nav = document.querySelector(".nav-mobile");
+  const navLinks = document.querySelectorAll(".nav-mobile li");
 
-    burger.addEventListener('click', () => {
-
-        nav.classList.toggle('nav-active');
-    });
-}
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+  });
+};
 
 navSlide();
