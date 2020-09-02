6fdcf0d05d3a500af28d52327b9a00c88c0d57cb
Juan Andrés Morena <thewolfmodzyt@gmaill.com>
2020-09-02 15:39:14 -0300

    BotonFrame

--- Proyectos/Panel.php
+++ Proyectos/Panel.php
@@ -90,6 +90,13 @@
                   >
                 </li>
                 <li>
+                  <a href="/ExpoferiaOnline/Proyectos/EditarProyectos.php" style="<?php echo $prof ?>"
+                    ><button class="botonPanel">
+                      <i class="fa">&#xf0fe;</i> Unirse a Proyectos
+                    </button></a
+                  >
+                </li>
+                <li>
                   <a href="/ExpoferiaOnline/Admin.html" style="<?php echo $admin ?>"
                     ><button class="botonPanel">
                       <i class="fa">&#xf0c0;</i> Administrar Usuarios
