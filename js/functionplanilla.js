5ff7ffa0887282b45be97cb3695b2e3843901370
EmiSan <s.faraldo122@gmail.com>
2020-09-16 19:50:44 -0300

    planilla funcional

--- js/functionplanilla.js
+++ js/functionplanilla.js
@@ -6,24 +6,18 @@ $( document ).ready(function() {
           
           $('.Planilla').html(content);
           
-          
-          carousel();
-          slideIndex=0;
-          showSlides(slideIndex);
-
-    
+         
+       
+        
       }
     });
-    // MODAL que carga imagenes
-    //-- Modal Agrandar Fotos -->
-    
 });
 var slideIndex = 0;
+var slideIndexVid = 0;
+
 function agrando(foto){
   var modal = document.getElementById('myModal');
-
   var img = document.getElementById('foto'+foto);
-
   var modalImg = document.getElementById('foto');
   img.onclick = function(){
     modal.style.display = 'block';
@@ -31,43 +25,86 @@ function agrando(foto){
   }
   var span = document.getElementsByClassName('close')[0];
 }
+
+function cerrarModal() { 
+  document.getElementById('myModal').style.display = 'none';
+}
+
+function getVideo(url,id){
+  var regExp = /^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+  var match = url.match(regExp);
+  if (match && match[2].length == 11) {
+      document.getElementById('video'+id).src = 'https://www.youtube.com/embed/'+match[2];
+  } else {
+      document.getElementById('divideo').style.visibility = 'hidden';
+  }
+  
+}
+//Slides para las Imagenes
+function slides(){
+  carousel();
+  showSlides(slideIndex);
+}
+
+
 function plusSlides(n) {
   showSlides(slideIndex += n);
-  }
+}
 
-  function currentSlide(n) {
+function currentSlide(n) {
   showSlides(slideIndex = n);
-  }
+}
 
-  function showSlides(n) {
+function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-  slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-  dots[i].className = dots[i].className.replace(" active", "");
+      slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
-
-  }
-
-  function carousel() {
-    var i;
-    var x = document.getElementsByClassName("mySlides");
-    for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
-    x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 8000); // Cambia la imagen cada 8 segundos
-  }
-  function cambiar(){
-    var pdrs = document.getElementById('file-upload').files[0].name;
-    document.getElementById('info').innerHTML = pdrs;
+  
 }
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 8000); // Cambia la imagen cada 8 segundos
+}
+
+//Slides para los videos
+
+function slidesVid(){
+  showSlidesVid(slideIndexVid);
+}
+
+
+function plusSlidesVid(n) {
+  showSlidesVid(slideIndexVid += n);
+}
+
+function currentSlideVid(n) {
+  showSlidesVid(slideIndexVid = n);
+}
+
+function showSlidesVid(n) {
+  var i;
+  var slides = document.getElementsByClassName("VideoSlides");
+  if (n > slides.length) {slideIndexVid = 1}
+  if (n < 1) {slideIndexVid = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slideIndexVid-1].style.display = "block";
+  
+}
+
+
+
