5f4c120462d46dd0629534ae34e6a29532244163
ElTomy <tomassosa1303@gmail.com>
2020-09-17 16:42:33 -0300

    guardar imagenes 1.1

--- Proyectos/uploadIMG.php
+++ Proyectos/uploadIMG.php
@@ -7,6 +7,8 @@ $idproyecto = $mysqli->query($sql);
 
 $ruta = 'proyecto' .$idproyecto; 
 
+$Contador;
+$nombre = "IMG".$Contador;
 $target_dir = $ruta;
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
@@ -39,6 +41,7 @@ if ($uploadOk == 0) {
 } else {
   if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
     echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    $Contador++;
   } else {
     echo "Sorry, there was an error uploading your file.";
   }
