fc15a8a36da8e296bd9d1b12121774054aa0346a
sasehara2018 <sasehara0@gmail.com>
2020-10-06 15:22:22 -0300

    tynimce

--- Proyectos/PlanillaEditable.php
+++ Proyectos/PlanillaEditable.php
@@ -14,14 +14,43 @@
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script src="tinymce/tinymce.min.js"></script>
     <script>
       tinymce.init({
-        selector: 'textarea#editor',
-        skin: 'bootstrap',
-        plugins: 'lists, link, image, media',
-        toolbar: 'h1 h2 bold italic strikethrough blockquote bullist numlist backcolor | link image media | removeformat help',
-        menubar: false
+        selector: '#descripcionLarga_Proyecto',
+        plugins: 'lists, link, image, media, code',
+  toolbar: 'h1 h2 bold italic strikethrough blockquote bullist numlist backcolor undo redo | link image media | code removeformat help',
+  // enable title field in the Image dialog
+  image_title: true, 
+  // enable automatic uploads of images represented by blob or data URIs
+  automatic_uploads: true,
+  // add custom filepicker only to Image dialog
+  file_picker_types: 'image',
+  file_picker_callback: function(cb, value, meta) {
+    var input = document.createElement('input');
+    input.setAttribute('type', 'file');
+    input.setAttribute('accept', 'image/*');
+
+    input.onchange = function() {
+      var file = this.files[0];
+      var reader = new FileReader();
+      
+      reader.onload = function () {
+        var id = 'blobid' + (new Date()).getTime();
+        var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+        var base64 = reader.result.split(',')[1];
+        var blobInfo = blobCache.create(id, file, base64);
+        blobCache.add(blobInfo);
+
+        // call the callback and populate the Title field with the file name
+        cb(blobInfo.blobUri(), { title: file.name });
+      };
+      reader.readAsDataURL(file);
+    };
+    
+    input.click();
+  }
+        
         });
     </script>
 
