696568741c783926ef553ec2b751ba40f2a2399a
sasehara2018 <sasehara0@gmail.com>
2020-10-07 18:27:59 -0300

    algunos arreglos menores

--- js/functionPlanillaEditable.js
+++ js/functionPlanillaEditable.js
@@ -9,3 +9,24 @@ function aprobar(id){
        });
      
 };
+function ActualizoPlanilla(){
+  var nombreproyecto = document.getElementById('user');
+  var dcorta = document.getElementById('descripcionCorta_Proyecto');
+  var dlarga = document.getElementById('descripcionLarga_Proyecto');
+  var mlink = document.getElementById('link');
+  var idp = $("#user").data("idp");
+
+    $.ajax({
+    url:"ActualizoPlanilla.php", 
+    type: "post", 
+    data: {idp: idp, nombre: nombreproyecto, dcorta: dcorta, dlarga: dlarga, mlink: mlink},
+    success:function(content){
+        alert("el resultado es "+content);
+      }
+     });
+    
+}
+function hfindex() {
+  $("#header").load("../HeaderFooter/header.php");
+  $("#footer").load("../HeaderFooter/footer.html");
+}
