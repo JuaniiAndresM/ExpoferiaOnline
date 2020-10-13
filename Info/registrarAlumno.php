0091168a4b2169b9c2476ec942c22048ed0e9026
Adrian Omero <torresmacciosrl@gmail.com>
2020-10-13 17:21:21 -0300

    hola

--- Info/registrarAlumno.php
+++ Info/registrarAlumno.php
@@ -1,12 +1,12 @@
-<?php include ("contacto.php");?>
-<?php   
+
+<?php 
     include "../Form/conexion.php";
     if(isset($_POST['usuario']) && $_POST['password']){
     $sql = "SELECT * FROM solicitud_usuario WHERE Usuario = '".$_POST['usuario']."'";
     $result = $mysqli -> query($sql);
     $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
     if (isset($ss['Usuario'])){ 
-       echo 1;                 
+
     }else{
         $sql = "SELECT * FROM usuario WHERE Usuario = '".$_POST['usuario']."'";
         $result = $mysqli -> query($sql);
@@ -22,19 +22,6 @@
             if (isset($ss['Usuario'])){
                 header('Location: ../Info/contacto.php?eP');       
             }else{
-                $sql = "SELECT * FROM datosProyecto WHERE Titulo = '".$_POST['nomproy']."'";
-                $result = $mysqli -> query($sql);
-                $ss = mysqli_fetch_array($result, MYSQLI_ASSOC); 
-                if (isset($ss['Titulo'])){
-                    header('Location: ../Info/contacto.php?ePe');       
-        
-                }else{
-                    $sql = "SELECT * FROM solicitud_usuario WHERE Titulo_Proyecto = '".$_POST['nomproy']."'";
-                    $result = $mysqli -> query($sql);
-                    $ss = mysqli_fetch_array($result, MYSQLI_ASSOC); 
-                    if (isset($ss['Titulo_Proyecto'])){
-                        header('Location: ../Info/contacto.php?ePe');   
-                    }else{
                         $usuario = '"'.$mysqli->real_escape_string($_POST['usuario']).'"';
                         $contra = '"'.$mysqli->real_escape_string($_POST['password']).'"';
                         $email = '"'.$mysqli->real_escape_string($_POST['email']).'"';
@@ -48,24 +35,11 @@
                             
                     }
                     if($insert_row==true){
-                        ?>
-                        <script>
-                        alert("Todo bien");
-                        </script>
-                        <?php
-
-                        
+                        echo "1";
                     }else{
-                        ?>
-                        <script>
-                        alert("Todo bien");
-                        </script>
-                         <?php
+
                     }
                  }
             }
-        } 
-    } 
-}    
-    $mysqli -> close();
+        }  
 ?>
