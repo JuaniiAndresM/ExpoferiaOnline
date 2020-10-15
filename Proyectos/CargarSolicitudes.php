565735b82a4f2fa8b0ba841ad516b8c6af00bea1
EmiSan <s.faraldo122@gmail.com>
2020-10-15 11:28:52 -0300

    arreglo

--- Proyectos/CargarSolicitudes.php
+++ Proyectos/CargarSolicitudes.php
@@ -66,7 +66,7 @@ if($result){
                         id='comment'
                       ></textarea>
                       
-                      <button class='Enviar' onclick='deshabilitar(".$sqlsolicitudes['idSoli_Usuario'].")'><i class='fa'>&#xf1d8;</i> Enviar</button>
+                      <button class='Enviar' onclick='noaprobado(".$sqlsolicitudes['idSoli_Usuario'].")'><i class='fa'>&#xf1d8;</i> Enviar</button>
                     </div>
               </div>
               <hr>
@@ -113,7 +113,7 @@ if($result){
                           placeholder='Ingrese Comentario'
                           id='commentPROF'
                         ></textarea>
-                        <button class='Enviar' onclick='desaprobadoPROF(".$sqlsolicitudes['idSoliProf'].")' ><i class='fa'>&#xf1d8;</i> Enviar</button>
+                        <button class='Enviar' onclick='noaprobadoPROF(".$sqlsolicitudes['idSoliProf'].")' ><i class='fa'>&#xf1d8;</i> Enviar</button>
                       </div>
                 </div>
                 <hr>
