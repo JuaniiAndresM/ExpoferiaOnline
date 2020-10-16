9d09b6fcbd67e3127eca79761ad0ad98488240d4
Agus Aldunate <SU-CORREO-ELECTRONICO>
2020-10-15 21:27:38 -0300

    arreglos video

--- Proyectos/SuboVideos.php
+++ Proyectos/SuboVideos.php
@@ -5,10 +5,16 @@
     $video =$_POST['video'];
     $idproyecto = $_POST['idp'];
     $sql= "INSERT INTO videos (url, idProyecto) VALUES ('".$video."', ".$_POST['idp'].");";
+   
     
     if($mysqli->query($sql)){
         echo "todo bien con ". $sql;
     }else echo "todo mal con este ".$sql;
+    
+    if($video == ""){
+      $sql="DELETE FROM videos WHERE idProyecto='".$_POST['idp']."'";
+      $result = $mysqli -> query($sql);
+    }
     return;
 
 ?>
