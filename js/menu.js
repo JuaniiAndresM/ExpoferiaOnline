f84d851b5fd27d7d434311ab61c83c6e1142ea93
Juan Andrés Morena <64050263+JuaniiAndresM@users.noreply.github.com>
2020-10-20 18:18:38 -0300

    Acerca

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 function mobile() {
   if (document.getElementById("nav-mobile").style.transform == "translateY(0%)") {
-      document.getElementById("nav-mobile").style.transform = "translateY(-105%)";
+      document.getElementById("nav-mobile").style.transform = "translateY(-120%)";
       document.getElementById("open").style.display ="block";
       document.getElementById("close").style.display ="none";
       document.body.style.overflowY = "scroll";
