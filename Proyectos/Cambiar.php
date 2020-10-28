f3a112da935b9db75fe264ba02e67df5ce49025f
MatiRasnik <MatiRasnik@users.noreply.github.com>
2020-10-28 09:22:24 -0300

    panel y modificar usuarios

--- Proyectos/Cambiar.php
+++ Proyectos/Cambiar.php
@@ -2,39 +2,36 @@
 include 'verificosesion.php';
 include '../Form/conexion.php';
 
-$sql = "SELECT * FROM usuario WHERE Usuario = '" . $_SESSION['Usuario'] . "'";
+$sql = "UPDATE usuario SET Password = '" .$_POST['contra']."', Email = '" .$_POST['mail']."', Nombre ='" .$_POST['nombre']."',Apellido = '" .$_POST['apellido']."' WHERE usuario ='". $_SESSION['Usuario']."'";
+$result =  $mysqli->query($sql);
+
+$sql = "SELECT * FROM usuario WHERE Usuario = '" . $_POST['user'] . "'";
 $result = $mysqli->query($sql);
 $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
 if (isset($ss['Usuario'])) {
-    echo 1;
+
 } else {
-    $sql = "SELECT * FROM solicitud_usuario WHERE Usuario = '" . $_SESSION['Usuario'] . "'";
+    $sql = "SELECT * FROM solicitud_usuario WHERE Usuario = '" . $_POST['user'] . "'";
     $result = $mysqli->query($sql);
     $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
     if (isset($ss['Usuario'])) {
-        echo 1;
+
     } else {
-        $sql = "SELECT * FROM solicitud_profesor WHERE Usuario = '" . $_SESSION['Usuario'] . "'";
+        $sql = "SELECT * FROM solicitud_profesor WHERE Usuario = '" . $_POST['user'] . "'";
         $result = $mysqli->query($sql);
         $ss = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
         if (isset($ss['Usuario'])) {
-            echo 1;
+
         } else {
-            "UPDATE usuario SET Usuario = '" .$_POST['user']."' WHERE usuario ='". $_SESSION['Usuario']."'";
+            $sql = "UPDATE usuario SET Usuario = '" .$_POST['user']."' WHERE usuario ='". $_SESSION['Usuario']."'";
+            $resultado = $mysqli->query($sql);
+            echo "#funciona";
         }
     }
 }                 
-    
-"UPDATE usuario SET Password = '" .$_POST['contra']."' WHERE usuario ='". $_SESSION['Usuario']."'";
-
-"UPDATE usuario SET Email = '" .$_POST['mail']."' WHERE usuario ='". $_SESSION['Usuario']."'";
-  
-"UPDATE usuario SET Nombre ='" .$_POST['nombre']."' WHERE usuario ='".$_SESSION['Usuario']."'";
-  
-"UPDATE usuario SET Apellido = '" .$_POST['apellido']."' WHERE usuario ='".$_SESSION['Usuario']."'";
-
-header('Location: ModificarUsuario.php');
+include '../Form/CerrarSesion.php';
+header('Location: ../Form/login.php');
 
 ?>
