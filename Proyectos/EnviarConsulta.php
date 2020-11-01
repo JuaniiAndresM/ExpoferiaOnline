7c88455e1a696388ce12a5817f812cc56d2b7cc0
ElTomy <tomassosa1303@gmail.com>
2020-11-01 16:00:32 -0300

    soy boludo

--- Proyectos/EnviarConsulta.php
+++ Proyectos/EnviarConsulta.php
@@ -3,5 +3,5 @@ include '../form/conexion.php';
 $email = '"' . $mysqli->real_escape_string($_POST['mail']) . '"';
 $consulta = '"' . $mysqli->real_escape_string($_POST['consulta']) . '"';
 $insert_row = $mysqli->query("INSERT INTO solicitud_mensajes  (Email, Mensaje) VALUES($email, $consulta)");
-header('Location: consultar.php');  
+header('Location: Consultar.php');  
 ?>
